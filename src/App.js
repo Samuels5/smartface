ab3e9a1bb75729267f952370fa0c02063873e19f
Samuels5 <samimule20@gmail.com>
2024-11-10 17:56:57 +0300

    finilise

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ class App extends Component {
     //////////////////////////////////////////////////////////////////////////////////////////////////
 
     // Your PAT (Personal Access Token) can be found in the Account's Security section
-    const PAT = "8c694cbc06244d128411b68082b403e3";
+    // const PAT = "8c694cbc06244d128411b68082b403e3";
     // Specify the correct user_id/app_id pairings
     // Since you're making inferences outside your app's scope
     const USER_ID = "clarifai";
@@ -64,63 +64,32 @@ class App extends Component {
     // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
     ///////////////////////////////////////////////////////////////////////////////////
 
-    const raw = JSON.stringify({
-      user_app_id: {
-        user_id: USER_ID,
-        app_id: APP_ID,
-      },
-      inputs: [
-        {
-          data: {
-            image: {
-              url: IMAGE_URL,
-              // "base64": IMAGE_BYTES_STRING
-            },
-          },
-        },
-      ],
-    });
+    // const raw = JSON.stringify({
+    //   user_app_id: {
+    //     user_id: USER_ID,
+    //     app_id: APP_ID,
+    //   },
+    //   inputs: [
+    //     {
+    //       data: {
+    //         image: {
+    //           url: IMAGE_URL,
+    //           // "base64": IMAGE_BYTES_STRING
+    //         },
+    //       },
+    //     },
+    //   ],
+    // });
 
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        Authorization: "Key " + PAT,
-      },
-      body: raw,
-    };
+    // const requestOptions = {
+    //   method: "POST",
+    //   headers: {
+    //     Accept: "application/json",
+    //     Authorization: "Key " + PAT,
+    //   },
+    //   body: raw,
+    // };
 
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
-
-    // fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/versions/" + MODEL_VERSION_ID + "/outputs", requestOptions)
-    //     .then(response => response.json())
-    //     .then(result => {
-    //       this.setbot(this.resposnsible(result))
-
-    //         // const regions = result.outputs[0].data.regions;
-
-    //         // regions.forEach(region => {
-    //         //     // Accessing and rounding the bounding box values
-    //         //     const boundingBox = region.region_info.bounding_box;
-    //         //     const topRow = boundingBox.top_row.toFixed(3);
-    //         //     const leftCol = boundingBox.left_col.toFixed(3);
-    //         //     const bottomRow = boundingBox.bottom_row.toFixed(3);
-    //         //     const rightCol = boundingBox.right_col.toFixed(3);
-
-    //         //     region.data.concepts.forEach(concept => {
-    //         //         // Accessing and rounding the concept value
-    //         //         const name = concept.name;
-    //         //         const value = concept.value.toFixed(4);
-
-    //         //         console.log(`${name}: ${value} BBox: ${topRow}, ${leftCol}, ${bottomRow}, ${rightCol}`);
-
-    //         //     });
-    //         // });
-
-    //     })
-    //     .catch(error => console.log('error', error));https://smartfacebackend.vercel.app
     fetch("https://smartfacebackend-4.onrender.com/api/analyze", {
       method: "POST",
       // mode: "no-cors",
@@ -196,166 +165,3 @@ class App extends Component {
 }
 
 export default App;
-
-// import "./App.css";
-// import Navigation from "./components/Navigation";
-// import Logo from "./components/Logo";
-// import Imagelink from "./components/Imagelink";
-// import Rank from "./components/Rank";
-// import Sign from "./components/Sign";
-// import Register from "./components/Register";
-// import FaceRecognition from "./components/FaceRecognition";
-// import React, { Component } from "react";
-
-// class App extends Component {
-//   constructor() {
-//     super();
-//     this.state = {
-//       input: "",
-//       imgurl: "",
-//       box: {},
-//       route: "signin",
-//       issign: false,
-//     };
-//   }
-
-//   responsible = (data) => {
-//     // Check if data and its expected properties are defined
-//     if (!data || !data.outputs || data.outputs.length === 0) {
-//       console.error("Invalid response data:", data);
-//       return {}; // Return an empty object or handle the error appropriately
-//     }
-
-//     const myobject = data.outputs[0].data.regions[0].region_info.bounding_box;
-//     const image = document.getElementById("inputimage");
-//     const width = Number(image.width);
-//     const height = Number(image.height);
-//     console.log(width, height);
-
-//     return {
-//       top: height * myobject.top_row,
-//       left: width * myobject.left_col,
-//       right: width - width * myobject.right_col,
-//       bottom: height - height * myobject.bottom_row,
-//     };
-//   };
-
-//   setbot = (res) => {
-//     this.setState({ box: res });
-//   };
-
-//   oninputchange = (event) => {
-//     this.setState({ input: event.target.value });
-//   };
-
-//   onbuttonsubmit = () => {
-//     this.setState({ imgurl: this.state.input });
-
-//     const PAT = "8c694cbc06244d128411b68082b403e3";
-//     const USER_ID = "clarifai";
-//     const APP_ID = "main";
-//     const MODEL_ID = "face-detection";
-//     const MODEL_VERSION_ID = "6dc7e46bc9124c5c8824be4822abe105";
-//     const IMAGE_URL = this.state.input;
-//     console.log(IMAGE_URL);
-//     const raw = JSON.stringify({
-//       user_app_id: {
-//         user_id: USER_ID,
-//         app_id: APP_ID,
-//       },
-//       inputs: [
-//         {
-//           data: {
-//             image: {
-//               url: IMAGE_URL,
-//             },
-//           },
-//         },
-//       ],
-//     });
-
-//     const requestOptions = {
-//       method: "POST",
-//       headers: {
-//         Accept: "application/json",
-//         Authorization: "Key " + PAT,
-//         "Content-Type": "application/json",
-//       },
-//       body: raw,
-//     };
-
-//     fetch("http://localhost:5000/api/analyze", {
-//       method: "POST",
-//       headers: { "Content-Type": "application/json" },
-//       body: JSON.stringify({
-//         modelId: MODEL_ID,
-//         modelVersionId: MODEL_VERSION_ID,
-//         user_app_id: {
-//           user_id: USER_ID, // Replace with your actual user ID
-//           app_id: APP_ID, // Replace with your actual app ID
-//         },
-//         inputs: [
-//           {
-//             data: {
-//               image: {
-//                 url: IMAGE_URL,
-//               },
-//             },
-//           },
-//         ],
-//       }),
-//     })
-//       .then((response) => {
-//         if (!response.ok) {
-//           console.error("Error in response:", response);
-//           throw new Error(`HTTP error! Status: ${response.status}`);
-//         }
-//         return response.json();
-//       })
-//       .then((result) => {
-//         console.log("API Response:", result); // Log the full response
-//         this.setbot(this.responsible(result)); // Handle the result here
-//       })
-//       .catch((error) => {
-//         console.error("Error:", error);
-//         // Additional handling if needed
-//       });
-//   };
-
-//   onRouteChange = (to) => {
-//     if (to === "signin" || to === "register") {
-//       this.setState({ issign: false });
-//     } else {
-//       this.setState({ issign: true });
-//     }
-//     this.setState({ route: to });
-//   };
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <Navigation
-//           issign={this.state.issign}
-//           onRouteChange={this.onRouteChange}
-//         />
-//         {this.state.route === "home" ? (
-//           <div>
-//             <Logo />
-//             <Rank />
-//             <Imagelink
-//               oninputchange={this.oninputchange}
-//               onbuttonsubmit={this.onbuttonsubmit}
-//             />
-//             <FaceRecognition box={this.state.box} imgurl={this.state.imgurl} />
-//           </div>
-//         ) : this.state.route === "signin" ? (
-//           <Sign onRouteChange={this.onRouteChange} />
-//         ) : (
-//           <Register onRouteChange={this.onRouteChange} />
-//         )}
-//       </div>
-//     );
-//   }
-// }
-
-// export default App;
--- src/components/Imagelink.js
+++ src/components/Imagelink.js
@@ -8,7 +8,7 @@ const Imagelink = ({oninputchange, onbuttonsubmit}) => {
             <p className='f3'>{'This will detect faces in your pictures. give it a try.'}</p>
             <div className='center'>
                 <div className='center form pa4 br3 shadow-5'>
-                    <input className='f4 pa2 w-70 center' type='text' onChange={oninputchange}/>
+                    <input className='f4 pa2 w-70 center' type='text' placeholder='Insert image link' onChange={oninputchange}/>
                     <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick = {onbuttonsubmit}>Detect</button>
                 </div>
             </div>
